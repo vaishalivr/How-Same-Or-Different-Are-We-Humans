ec93ba2257f9f303b58b6e67e6fb6dfd972ca9b8
Vaishali Verma <65554439+vaishalivr@users.noreply.github.com>
2025-02-20 16:38:09 -0500

    put data into a new file for story one

--- src/components/storyOne/StoryOneSvgOne.svelte
+++ src/components/storyOne/StoryOneSvgOne.svelte
@@ -1,70 +1,11 @@
 <script>
-  let sentences = [
-    { text: "night sky past 4pm", x: 150, y: 150, rotation: -20, fill: 0.45 },
-    {
-      text: "seeing snow on some mornings",
-      x: 800,
-      y: 100,
-      rotation: 25,
-      fill: 0.6,
-    },
-    {
-      text: "woodern floored bathrooms",
-      x: 600,
-      y: 150,
-      rotation: 10,
-      fill: 0.75,
-    },
-    {
-      text: "freezing despite the sun shining",
-      x: 350,
-      y: 250,
-      rotation: 20,
-      fill: 1,
-    },
-    {
-      text: "clothes dryer in every house",
-      x: 200,
-      y: 360,
-      rotation: -20,
-      fill: 1,
-    },
-    {
-      text: "different units of measurements",
-      x: 300,
-      y: 500,
-      rotation: -20,
-      fill: 1,
-    },
-    {
-      text: "drinking water directly from kitchen tap",
-      x: 300,
-      y: 500,
-      rotation: -20,
-      fill: 1,
-    },
-    {
-      text: "kids playing indoor after school",
-      x: 800,
-      y: 300,
-      rotation: 20,
-      fill: 1,
-    },
-    {
-      text: "Boston Celtics, Boston Bruins, New England Patriots",
-      x: 600,
-      y: 520,
-      rotation: -10,
-      fill: 1,
-    },
-  ];
-
   import { onMount } from "svelte";
+  import sentences from "../../lib/data/sentencesData.js";
 
   let height = 600;
   let textElement;
   let rectElement;
-  let bbPadding = 15;
+  let bbPadding = 8;
 
   function getBoundingBox(textElement, rectElement, fillRatio) {
     if (textElement && rectElement) {
